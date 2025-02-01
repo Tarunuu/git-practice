e89db6ef3241ffa24d57810a6684b289acc254c8
Tarun Budiredla <budiredlatarun@gmail.com>
2025-02-01 01:39:49 -0600

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
+echo "$PERSON1:: I am doing good ${PERSON2}. what is going on in life?"
+echo "${PERSON2}:: I have been practising devops projects and giving interviews $PERSON1"
