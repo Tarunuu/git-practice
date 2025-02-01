a61e272b4ebd1b2106fbc58b0721f993c6df5c7c
Tarun Budiredla <budiredlatarun@gmail.com>
2025-02-01 01:12:25 -0600

    changed the mistake

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,5 +2,5 @@
 
 echo "Ramesh:: Hi Suresh, How are you?"
 echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
-echo "Ramesh:: I am doing good suresh. waht's going on?"
+echo "Ramesh:: I am doing good suresh. what's going on in life?"
 echo "Suresh:: I have been practising devops projects and giving interviews"
