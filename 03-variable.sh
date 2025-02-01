bb6b141e80c52d268a36c57d4e24d826c3c030a5
Tarun Budiredla <budiredlatarun@gmail.com>
2025-02-01 01:30:54 -0600

    variables

--- 03-variable.sh
+++ 03-variable.sh
@@ -2,7 +2,7 @@
 PERSON1=Robin
 PERSON2=Suresh
 
-echo "$PERSON1:: Hi {$PERSON2}, How are you?"
-echo "{$PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
-echo "$PERSON1:: I am doing good {$PERSON2}. what is going on in life?"
-echo "{$PERSON2}:: I have been practising devops projects and giving interviews $PERSON1"
+echo "$PERSON1:: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
+echo "$PERSON1:: I am doing good ${PERSON2}. what is going on in life?"
+echo "${PERSON2}:: I have been practising devops projects and giving interviews $PERSON1"
