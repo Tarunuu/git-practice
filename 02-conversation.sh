fdd14c188d5f5bf7cd57b49d90874c68226b7fa3
Tarun Budiredla <budiredlatarun@gmail.com>
2025-02-01 01:08:58 -0600

    added convesation script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
+echo "Ramesh:: I am doing good suresh. waht's going on?"
+echo "Suresh:: I have been practising devops projects and giving interviews"
