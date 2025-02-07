1be7d48e2c0415e4c4b3a740750f2a8d6874cdd0
Tarun Budiredla <budiredlatarun@gmail.com>
2025-02-07 11:34:06 -0600

    function

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
@@ -22,10 +22,10 @@ CHECK_ROOT(){
 VALIDATE (){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is.. $R FAILED $N" tee -a $LOG_FILE
+        echo -e "$2 is.. $R FAILED $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is.. $G SUCCESS $N" tee -a $LOG_FILE
+        echo -e "$2 is.. $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
