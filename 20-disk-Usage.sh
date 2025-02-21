3df0e15d23fe309035d6ccc18cc01592379616c0
Tarun Budiredla <budiredlatarun@gmail.com>
2025-02-20 23:33:47 -0500

    function

--- 20-disk-Usage.sh
+++ 20-disk-Usage.sh
@@ -6,4 +6,4 @@ DISK_THRESHOLD=5 #real projects, it is usually 75
 while IFS= read -r file #IFS,internal field seperator, empty it will ignore while space -r is for not to ignore special characters like /
 do
    echo $line
-done <<< $DISK_USAGE
+done <<< $DISK_USAG
